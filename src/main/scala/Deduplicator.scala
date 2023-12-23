6586a0eace70151ed4194a70afe8420fdc0b9b9d
yavuzdemir <ahmet.demir@cognira.com>
2023-12-23 22:46:19 +0300

    Add secret scanner

--- src/main/scala/Deduplicator.scala
+++ src/main/scala/Deduplicator.scala
@@ -1,5 +1,7 @@
 object Deduplicator extends App {
 
+  val password = "35169ee57a417474ce427e2c9a31d931a663dff0"
+  println(password)
   println(deduplicate(Array("1", "2", "3", "2")).mkString("Array(", ", ", ")"))
 
   def deduplicate(myArgs: Array[String]): Array[String] = {
